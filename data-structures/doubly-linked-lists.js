3f6c030e23ec83aeb6255bb33d3480070d01e07c
bradpreston <bap5393@gmail.com>
2023-04-13 14:51:44 -0400

    add shift to doubly linked list

--- data-structures/doubly-linked-lists.js
+++ data-structures/doubly-linked-lists.js
@@ -47,4 +47,18 @@ export class DoublyLinkedList {
 		this.length--;
 		return currentTail;
 	}
+	shift() {
+		if (this.length === 0) return null;
+		let currentHead = this.head;
+		if (this.length === 1) {
+			this.head = null;
+			this.tail = null;
+		} else {
+			this.head = currentHead.next;
+			this.head.prev = null;
+			currentHead.next = null;
+		}
+		this.length--;
+		return currentHead;
+	}
 }
