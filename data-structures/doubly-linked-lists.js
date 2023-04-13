d52be09fa1e20105a083810dce6d15104e3c455c
bradpreston <bap5393@gmail.com>
2023-04-13 14:24:07 -0400

    add push to doubly linked list

--- data-structures/doubly-linked-lists.js
+++ data-structures/doubly-linked-lists.js
@@ -12,4 +12,24 @@ export class DoublyLinkedList {
 		this.tail = null;
 		this.length = 0;
 	}
+	print() {
+		let current = this.head;
+		while (current) {
+			console.log(current.val);
+			current = current.next;
+		}
+	}
+	push(val) {
+		const newNode = new Node(val);
+		if (this.head === null) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			newNode.prev = this.tail;
+			this.tail = newNode;
+		}
+		this.length++;
+		return this;
+	}
 }
