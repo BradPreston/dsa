f09c313265f245b371f81c19faf3ed30e47307eb
bradpreston <bap5393@gmail.com>
2023-04-19 15:59:42 -0400

    export queue class

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -5,17 +5,10 @@ class Node {
 	}
 }
 
-class Queue {
+export class Queue {
 	constructor() {
 		this.first = null;
 		this.last = null;
 		this.size = 0;
-	}
-	print() {
-		let current = this.first;
-		while (current) {
-			console.log(current);
-			current = current.next;
-		}
 	}
 }
