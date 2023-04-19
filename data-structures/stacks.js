4141d2adb62de3f63b87c54089b6d130225a482c
bradpreston <bap5393@gmail.com>
2023-04-19 13:13:54 -0400

    formatted stack class

--- data-structures/stacks.js
+++ data-structures/stacks.js
@@ -1,16 +1,16 @@
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
 export class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
 	print() {
 		let current = this.first;
 		while (current) {
@@ -18,28 +18,28 @@ export class Stack {
 			current = current.next;
 		}
 	}
-  push(val) {
-    const newNode = new Node(val);
-    if (this.size === 0) {
-      this.first = newNode;
-      this.last = newNode;
-    } else {
-      let currentFirst = this.first;
-      this.first = newNode;
-      newNode.next = currentFirst;
-    }
-    this.size++;
-    return this;
-  }
-  pop() {
-    if (this.size === 0) return null;
-    const oldFirst = this.first;
-    if (this.size === 1) {
-      this.last = null;
-    }
-    this.first = oldFirst.next;
-    oldFirst.next = null;
-    this.size--;
-    return oldFirst;
-  }
+	push(val) {
+		const newNode = new Node(val);
+		if (this.size === 0) {
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			let currentFirst = this.first;
+			this.first = newNode;
+			newNode.next = currentFirst;
+		}
+		this.size++;
+		return this;
+	}
+	pop() {
+		if (this.size === 0) return null;
+		const oldFirst = this.first;
+		if (this.size === 1) {
+			this.last = null;
+		}
+		this.first = oldFirst.next;
+		oldFirst.next = null;
+		this.size--;
+		return oldFirst;
+	}
 }
