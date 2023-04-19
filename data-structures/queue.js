7b825644e963bca18ffb86d6d6b2026ca261dde8
bradpreston <bap5393@gmail.com>
2023-04-19 15:55:45 -0400

    add print method to queue class

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -11,4 +11,11 @@ class Queue {
 		this.last = null;
 		this.size = 0;
 	}
+	print() {
+		let current = this.first;
+		while (current) {
+			console.log(current);
+			current = current.next;
+		}
+	}
 }
